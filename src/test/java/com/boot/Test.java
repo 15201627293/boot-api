9784f8f8774d7954d1e10adaac528d64d1dad8db
sunxiaohu <sunxiaohu@foxgoing.com>
2021-01-04 18:13:29 +0800

    测试

--- src/test/java/com/boot/Test.java
+++ src/test/java/com/boot/Test.java
@@ -5,9 +5,9 @@ import cn.hutool.core.lang.Snowflake;
 import cn.hutool.core.util.CharsetUtil;
 import cn.hutool.core.util.IdUtil;
 import com.alibaba.fastjson.JSON;
+import com.boot.util.HttpUtil;
 
 import java.util.*;
-import java.util.concurrent.CountDownLatch;
 
 /**
  * @Classname Test
@@ -16,7 +16,17 @@ import java.util.concurrent.CountDownLatch;
  */
 public class Test {
 
+
+
     public static void main(String[] args) {
+        String url = "http://47.104.20.179:8848/nacos/v1/auth/users?accessToken=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJuYWNvcyIsImV4cCI6MTYwODAzMTEzOX0.sxsKv1jU8YhBwoXnau2Q7sYaTXoU3CEerD7EhGPucJg";
+
+
+        while (true) {
+            url += "&username="+"魏狗" + get32()+"&password=123456";
+            String s = HttpUtil.doPost(url);
+            System.out.println(s);
+        }
 
         //        cloneByStream();
 //        toStr();
@@ -35,25 +45,25 @@ public class Test {
 //        }
 
 
-        final Set<String> snowflakeSet = Collections.synchronizedSet(new HashSet<>());
-
-        final int count = 100;
-        final CountDownLatch latch = new CountDownLatch(count);
-        for (int i = 0; i < count; i++) {
-            new Thread(() -> {
-                snowflakeSet.add(snowflake32());
-
-                latch.countDown();
-            }).start();
-        }
-
-        try {
-            latch.await();
-        } catch (InterruptedException ignored) {
-        }
-
-        assert snowflakeSet.size() == count;
-        System.out.println(snowflakeSet);
+//        final Set<String> snowflakeSet = Collections.synchronizedSet(new HashSet<>());
+//
+//        final int count = 100;
+//        final CountDownLatch latch = new CountDownLatch(count);
+//        for (int i = 0; i < count; i++) {
+//            new Thread(() -> {
+//                snowflakeSet.add(snowflake32());
+//
+//                latch.countDown();
+//            }).start();
+//        }
+//
+//        try {
+//            latch.await();
+//        } catch (InterruptedException ignored) {
+//        }
+//
+//        assert snowflakeSet.size() == count;
+//        System.out.println(snowflakeSet);
     }
 
     public static String snowflake32() {
@@ -61,21 +71,16 @@ public class Test {
         return String.format("%d%s", System.currentTimeMillis(), snowflake.nextIdStr());
     }
 
-    public static String get32(){
+    public static String get32() {
         Random rand = new Random();
-        StringBuffer sb=new StringBuffer();
-        for (int i=1;i<=32;i++){
-            int randNum = rand.nextInt(9)+1;
-            String num=randNum+"";
-            sb=sb.append(num);
+        StringBuffer sb = new StringBuffer();
+        for (int i = 1; i <= 32; i++) {
+            int randNum = rand.nextInt(9) + 1;
+            String num = randNum + "";
+            sb = sb.append(num);
         }
-       return String.valueOf(sb);
+        return String.valueOf(sb);
     }
-
-
-
-
-
 
 
     public static void strToUnicode() {
@@ -84,6 +89,7 @@ public class Test {
         String unicode = Convert.strToUnicode(a);
         System.out.println(JSON.toJSONString(unicode));
     }
+
     public static void unicodeToStr() {
         String unicode = "\\u6211\\u662f\\u4e00\\u4e2a\\u5c0f\\u5c0f\\u7684\\u53ef\\u7231\\u7684\\u5b57\\u7b26\\u4e32";
         //结果为："我是一个小小的可爱的字符串"
